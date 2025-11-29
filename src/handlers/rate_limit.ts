e85dd7e2dda692b362b864654ecfbad7ba48126d
dannylit <itsdanlit@gmail.com>
2025-11-29 16:55:27 +0300

    add rate limiter

--- src/handlers/rate_limit.ts
+++ src/handlers/rate_limit.ts
@@ -1,16 +1,41 @@
-export const withRateLimit = rateLimit(4_000);
+export const withRateLimit = rateLimit(8_000);
+
+function formatTime(timestamp: number) {
+    const dateObj = new Date(timestamp);
+
+    const hours = dateObj.getHours().toString().padStart(2, "0");
+    const minutes = dateObj.getMinutes().toString().padStart(2, "0");
+    const seconds = dateObj.getSeconds().toString().padStart(2, "0");
+
+    return `${hours}:${minutes}:${seconds}`;
+};
 
 function rateLimit(delayMS: number) {
     let lastTime = 0;
+    let pendingPromise = false;
 
-    return async function <T>(fn: () => Promise<T>): Promise<T | null> {
+    return async function <T>(fn: () => Promise<T>): Promise<T> | null {
+        if (pendingPromise) {
+            console.log("call droped: pending...")
+            return null;
+        };
+
         const now = Date.now();
-        if (now - lastTime < delayMS) {
-            // console.log(`rate limited! Time left: ${(now - lastTime) / 1000}`);
-            return;
+        const timeSinceLastCall = now - lastTime;
+        if (timeSinceLastCall < delayMS) {
+            console.log(`rate limit! now: ${formatTime(now)} last: ${formatTime(lastTime)} Passed: ${now - lastTime}ms`);
+            return null;
+        };
+
+        pendingPromise = true;
+        try {
+            const resp = await fn();
+            lastTime = Date.now();
+            console.log("llm response at:", formatTime(lastTime));
+            return resp;
+        } finally {
+            pendingPromise = false;
         }
-        lastTime = now;
-        return await fn();
     };
 };
 
